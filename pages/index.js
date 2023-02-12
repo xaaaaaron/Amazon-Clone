31567e8b64a152ddf21bb5709b3339b715ac699f
xaaaron <aaron.alcarde@opensoftsolutions.com>
2023-02-12 21:09:35 +0800

    removed some pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import React, { useState, useEffect, useRef } from 'react';
 import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'

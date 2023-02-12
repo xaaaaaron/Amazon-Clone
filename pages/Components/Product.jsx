0853493d073777cee65a67b730bb0315c1a4a2be
xaaaron <aaron.alcarde@opensoftsolutions.com>
2023-02-12 21:05:27 +0800

    fix image alt

--- pages/Components/Product.jsx
+++ pages/Components/Product.jsx
@@ -14,7 +14,7 @@ const Product = ({id, title, price, description, category, image}) => {
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
-            <Image className="my-auto mx-auto" src={image} height={200} width={200} objectFit="contain" />
+            <Image className="my-auto mx-auto" src={image} height={200} width={200} objectFit="contain" alt="text" />
             <h4 className="my-3">{title}</h4>
             <div className="flex">
                 {Array(rating)
